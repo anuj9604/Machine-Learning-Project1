1541c34ead960f32dc60162bf8671593600c5e77
anuj9604 <newanuj1996@gmail.com>
2022-07-12 06:20:45 +0530

    setup file added

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from typing import List
 
 ##declaring variables for setup function
 PROJECT_NAME="housing-predictor"
-VERSION="0.0.1"
+VERSION="0.0.2"
 AUTHOR="Anuj Khare"
 DESCRIPTION="This is a very rudimentary project"
 REQUIREMENT_FILE_NAME="requirements.txt"
